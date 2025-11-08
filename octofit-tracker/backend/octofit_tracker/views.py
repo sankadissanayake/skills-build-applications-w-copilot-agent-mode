5738d51c60a8c59685a741aaaae3ce9a5442b332
Sanka Dissanayake <87465779+sankadissanayake@users.noreply.github.com>
2025-11-08 18:12:27 +0000

    Refactor api_root to use reverse for endpoint URLs and remove unnecessary environment variable handling

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -25,18 +25,12 @@ class LeaderboardViewSet(viewsets.ModelViewSet):
     queryset = Leaderboard.objects.all()
     serializer_class = LeaderboardSerializer
 
-import os
 @api_view(['GET'])
 def api_root(request, format=None):
-    codespace_name = os.environ.get('CODESPACE_NAME')
-    base_url = request.build_absolute_uri('/')
-    if codespace_name:
-        base_url = f'https://{codespace_name}-8000.app.github.dev/'
-    api_url = base_url + 'api/'
     return Response({
-        'users': api_url + 'users/',
-        'teams': api_url + 'teams/',
-        'activities': api_url + 'activities/',
-        'workouts': api_url + 'workouts/',
-        'leaderboard': api_url + 'leaderboard/',
+        'users': reverse('user-list', request=request, format=format),
+        'teams': reverse('team-list', request=request, format=format),
+        'activities': reverse('activity-list', request=request, format=format),
+        'workouts': reverse('workout-list', request=request, format=format),
+        'leaderboard': reverse('leaderboard-list', request=request, format=format),
     })
